1c25e649b881286904f7d88e0490e53336279ac3
Justin Varga <50709305+Justopian@users.noreply.github.com>
2020-02-28 09:19:11 -0500

    Update War.java

--- War.java
+++ War.java
@@ -5,5 +5,32 @@ import java.util.*;
 
 public class War
 {
+	public class WarHand extends Hand
+	{
+		public void play()
+		{
+			Scanner reader = new Scanner(System.in);
+			final String menu = "Choose an option:\n[0]: Play\n[1]: Check Cards\n[2]: QUIT";
+			
+			for ()
+			{
+				
+			}
+		}
+	}
 	
+	public static void main(String[] args)
+	{
+		// Initialize necessary variables
+		Scanner reader = new Scanner(System.in);
+		
+		WarHand[] players = new WarHand[2];
+		players[1] = 
+		
+		// Begin Turns
+		while (victor.equals(null))
+		{
+			
+		}
+	}
 }
