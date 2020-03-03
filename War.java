29601202abe65ee5e55fa8ed345c4e623e075840
Justin Varga <50709305+Justopian@users.noreply.github.com>
2020-03-03 14:01:23 -0500

    Update War.java

--- War.java
+++ War.java
@@ -6,11 +6,19 @@ import java.util.*;
 public class War
 {
 	public class WarHand extends Hand
-	{
+	{	
+		private Card activeCard;
+		
+		public WarHand(String n, Deck d)
+		{
+			super(n, 26, d);
+		}
+		
 		public void play()
 		{
-			Scanner reader = new Scanner(System.in);
 			final String menu = "Choose an option:\n[0]: Play\n[1]: Check Cards\n[2]: QUIT";
+			
+			Scanner reader = new Scanner(System.in);
 			int input;
 			
 			do
@@ -20,15 +28,17 @@ public class War
 				switch(input)
 				{
 					case 0: // Play
-						
+						playWar();
 						break;
 						
 					case 1: // Check Cards
-						
+						System.out.println(toString());
 						break;
 						
 					case 2:	// Quit
+						System.out.println("Have a nice day!");
 						System.exit(0);
+						break;
 						
 					default: // Invalid Input
 						System.out.println("Please input 0, 1, or 2.");
@@ -36,7 +46,18 @@ public class War
 				}
 			}
 			while(false);
+		}
+		
+		public void playWar()
+		{
+			activeCard = hand.get(0);
 			
+			System.out.println(name + " drew " + activeCard.toString());
+		}
+		
+		public Card getActiveCard()
+		{
+			return activeCard;
 		}
 	}
 	
@@ -45,13 +66,32 @@ public class War
 		// Initialize necessary variables
 		Scanner reader = new Scanner(System.in);
 		
+		// Create Players and Deck
+		Deck warDeck = new Deck();
 		WarHand[] players = new WarHand[2];
-		players[1] = 
+		players[0] = new WarHand("Opponent", warDeck);
+		// TODO: try-catch for valid input
+		System.out.print("Please input the player\'s name: ");
+		String playerName = reader.next();
+		players[1] = new WarHand(playerName, warDeck);
 		
-		// Begin Turns
-		while (victor.equals(null))
+		// Play the game
+		for (int i = 0 ; players[0].size() > 0 && players[1].size() > 0 ; i++)
 		{
 			
 		}
+		
+		// Declare the victor
+		WarHand victor;
+		if (players[0].size() > 0)
+		{
+			victor = players[0];
+		}
+		else
+		{
+			victor = players[1];
+		}
+		
+		System.out.println(victor.getName() + " won with " + victor.size() + " cards remaining!");
 	}
 }
