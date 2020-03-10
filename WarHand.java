47a7f6f17966e471afa4c282b887fdec878d84bd
Justin Varga <50709305+Justopian@users.noreply.github.com>
2020-03-10 19:00:00 -0400

    Update WarHand.java

--- WarHand.java
+++ WarHand.java
@@ -18,35 +18,6 @@ public class WarHand extends Hand
 		activeCard.play();
 	}
 	
-	public void add(Card c) // If you win the drawing match
-	{
-		hand.add(c);
-	}
-	
-	public void add(ArrayList<Card> cs)
-	{
-		for (Card c : (Card[])cs.toArray())
-		{
-			add(c);
-		}
-	}
-	
-	public Card remove()
-	{
-		return hand.remove(0);
-	}
-	
-	public Card remove(int index)
-	{
-		if (index > size() || index < 0)
-		{
-			System.out.println("WarHand.remove() : index out of bounds : " + index);
-			return null;
-		}
-		
-		return hand.remove(index);
-	}
-	
 	public Card getActiveCard()
 	{
 		return activeCard;
